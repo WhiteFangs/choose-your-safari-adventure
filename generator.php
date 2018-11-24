806504485bc9e1553446e1e30801638cc73d74be
WhiteFangs <white-fangs@hotmail.fr>
2018-11-24 15:29:52 +0100

    Added Twine generator

---
+++ generator.php
@@ -0,0 +1,87 @@
+<?php
+
+$visualDebug = false;
+$twinDebug = true;
+
+include("./graph.php");
+$root = $_SERVER['DOCUMENT_ROOT'];
+$string = file_get_contents($root . "/divers/cyoa/data.json");
+$animals = json_decode($string, true);
+
+$pages = GetPages($animals);
+
+if($visualDebug){
+	foreach ($pages as $page) {
+		echo $page->name . " (" . $page->area . ")";
+		if($page->nextPages != null){
+			foreach ($page->nextPages as $next) {
+				echo "->" . $next->name . " (" . $next->area . ")";
+				echo "<br>";
+			}
+		}
+	}
+}
+
+if($twinDebug){
+	function GetPositions($key){
+		$initialX = 1000;
+		$initialY = 100;
+		$xArray4 = array(-400, -200, 200, 400);
+		$xArray8 = array(-800, -600, -400, -200, 200, 400, 600, 800);
+		$positionX = 0;
+		$positionY = 0;
+		switch ($key) {
+			case 0:
+				$positionX = $initialX;
+				$positionY = $initialY;
+				break;
+			case 1:
+				$positionX = $initialX - 200;
+				$positionY = $initialY + 200;
+				break;
+			case 2:
+				$positionX = $initialX + 200;
+				$positionY = $initialY + 200;
+				break;
+			default:
+				$positionInLevel = ($key - 3) % 12;
+				$positionX = $initialX;
+				if($positionInLevel < 4){
+					$positionX += $xArray4[$positionInLevel];
+					$positionY = $initialY + 400 + ((floor(($key - 3) / 12) * 2) *200);
+				}else{
+					$positionX += $xArray8[$positionInLevel - 4];
+					$positionY = $initialY + 400 + (((floor(($key - 3) / 12) * 2) + 1) *200);
+				}
+				break;
+		}
+		return (object)array("x" => $positionX, "y" => $positionY);
+	}
+	?>
+
+	<tw-storydata name="Test" startnode="1" creator="Twine" creator-version="2.2.1" ifid="CD3FE479-0DA0-43AD-8F87-75DBDE871DD4" zoom="1" format="SugarCube" format-version="2.21.0" options="" hidden>
+	<style role="stylesheet" id="twine-user-stylesheet" type="text/twine-css"></style>
+	<script role="script" id="twine-user-script" type="text/twine-javascript"></script>
+
+	<?php
+	foreach ($pages as $key => $page) {
+		$positions = GetPositions($key);
+		?>
+		<tw-passagedata pid="<?php echo $key+1; ?>" name="<?php echo $page->name; ?>" tags="" position="<?php echo $positions->x . "," . $positions->y; ?>" size="100,100">
+		<?php 
+		echo $page->name;
+		echo "<br>";
+		echo $page->area;
+		echo "<br>";
+		if($page->nextPages != null){
+			foreach ($page->nextPages as $next) {
+				echo "Go [[". $next->area ."|" . $next->name . "]]";
+				echo "<br>";
+			}
+		}
+		echo "</tw-passagedata>";
+	}
+	echo "</tw-storydata>";
+}
+
+?>
--- graph.php
+++ graph.php
@@ -1,16 +1,11 @@
 <?php
 
-$visualDebug = false;
-
-$root = $_SERVER['DOCUMENT_ROOT'];
-$string = file_get_contents($root . "/divers/cyoa/data.json");
-$animals = json_decode($string, true);
-
 function GetAnimalPage($animals, $previousPages){
 	$animal = $animals[array_rand($animals)];
 	$areas = $animal["areas"];
 	return (object) array(
-		"name" => $animal["name"],
+		"name" => $animal["name"] . uniqid(),
+		"title" => $animal["name"],
 		"animal" => $animal,
 		"nextPages" => $previousPages,
 		"area" => $areas[array_rand($areas)] // if rarity == 3, 80% chance ranger called
@@ -21,70 +16,62 @@ function MapPagesToNextPages($page){
 	return (object)array("name" => $page->name, "area" => $page->area);
 }
 
-$pages = array();
-$endPage = (object) array(
-	"name" => "EndPage",
-	"animal" => null,
-	"nextPages" => null,
-	"area" => "building"
-	);
-$pages[] = $endPage;
-$previousPages = array_map("MapPagesToNextPages", $pages);
+function GetPages($animals){
+	$pages = array();
+	$endPage = (object) array(
+		"name" => "EndPage",
+		"title" => "THE END",
+		"animal" => null,
+		"nextPages" => null,
+		"area" => "building"
+		);
+	$pages[] = $endPage;
+	$previousPages = array_map("MapPagesToNextPages", $pages);
 
-$graphLevelCounter = 0; // 0 is 2 options, 1 is 4, 2 is 8, 3 is 4. So odd $graphLevelCounter is 4 options and even is 8.
-$totalGraphLevels = 100; // we want to finish with an odd $graphLevelCounter so we choose an even $totalGraphLevels
-for ($graphLevelCounter; $graphLevelCounter < $totalGraphLevels; $graphLevelCounter++) { 
-	$previousPagesNb = count($previousPages);
-	shuffle($previousPages);
-	$newPages = array();
-	if($previousPagesNb < 8){
-		$newPagesNb = $previousPagesNb * 2;
-		for($i = 0; $i < $newPagesNb; $i++){
-			// usual case $previousPagesNb == 4, so $newPagesNb == 8, we want to have all possible combinations of $previousPages(A,B,C,D) as 2 $nextPages
-			// so 1=>AB, 2=>BC, 3=>CD, 4=>DA, then after 4, 5=>AC, 6=>BD, then fill the rest 7=>CA, 8=>DB
-			// special cases are $previousPagesNb == 2 or 1, if 2 we take the 2 available everytime, if 1 just give the only possibility
-			$secondPageToChoose = $newPagesNb == 8 ? ($i + ($i < 4 ? 1 : 2)) % $previousPagesNb : ($i + 1) % $previousPagesNb;
-			$newPages[] = GetAnimalPage($animals, $previousPagesNb > 1 ? array($previousPages[$i % $previousPagesNb], $previousPages[$secondPageToChoose]) : $previousPages);
+	$graphLevelCounter = 0; // 0 is 2 options, 1 is 4, 2 is 8, 3 is 4. So odd $graphLevelCounter is 4 options and even is 8.
+	$totalGraphLevels = 10; // we want to finish with an odd $graphLevelCounter so we choose an even $totalGraphLevels
+	for ($graphLevelCounter; $graphLevelCounter < $totalGraphLevels; $graphLevelCounter++) { 
+		$previousPagesNb = count($previousPages);
+		shuffle($previousPages);
+		$newPages = array();
+		if($previousPagesNb < 8){
+			$newPagesNb = $previousPagesNb * 2;
+			for($i = 0; $i < $newPagesNb; $i++){
+				// usual case $previousPagesNb == 4, so $newPagesNb == 8, we want to have all possible combinations of $previousPages(A,B,C,D) as 2 $nextPages
+				// so 1=>AB, 2=>BC, 3=>CD, 4=>DA, then after 4, 5=>AC, 6=>BD, then fill the rest 7=>CA, 8=>DB
+				// special cases are $previousPagesNb == 2 or 1, if 2 we take the 2 available everytime, if 1 just give the only possibility
+				$secondPageToChoose = $newPagesNb == 8 ? ($i + ($i < 4 ? 1 : 2)) % $previousPagesNb : ($i + 1) % $previousPagesNb;
+				$newPages[] = GetAnimalPage($animals, $previousPagesNb > 1 ? array($previousPages[$i % $previousPagesNb], $previousPages[$secondPageToChoose]) : $previousPages);
+			}
+		}else {
+			for($i = 0; $i < 4; $i++)
+				// we get A=>(0,1), B=>(2,3), C=>(4,5) and D=>(6,7)
+				$newPages[] = GetAnimalPage($animals, array($previousPages[$i * 2], $previousPages[$i * 2 + 1]));
 		}
-	}else {
-		for($i = 0; $i < 4; $i++)
-			// we get A=>(0,1), B=>(2,3), C=>(4,5) and D=>(6,7)
-			$newPages[] = GetAnimalPage($animals, array($previousPages[$i * 2], $previousPages[$i * 2 + 1]));
+		$pages = array_merge($pages, $newPages);
+		$previousPages = array_map("MapPagesToNextPages", $newPages);
 	}
+
+	// create 2 pages to link to 4 last
+	$newPages = array();
+	$newPages[] = GetAnimalPage($animals, array($previousPages[0], $previousPages[1]));
+	$newPages[] = GetAnimalPage($animals, array($previousPages[2], $previousPages[3]));
 	$pages = array_merge($pages, $newPages);
 	$previousPages = array_map("MapPagesToNextPages", $newPages);
-}
 
-// create 2 pages to link to 4 last
-$newPages = array();
-$newPages[] = GetAnimalPage($animals, array($previousPages[0], $previousPages[1]));
-$newPages[] = GetAnimalPage($animals, array($previousPages[2], $previousPages[3]));
-$pages = array_merge($pages, $newPages);
-$previousPages = array_map("MapPagesToNextPages", $newPages);
+	// create begin page
+	$beginPage = (object) array(
+		"name" => "beginPage",
+		"title" => "THE BEGINNING",
+		"animal" => null,
+		"nextPages" => $previousPages,
+		"area" => "building"
+		);
+	$pages[] = $beginPage;
 
-// create begin page
-$beginPage = (object) array(
-	"name" => "beginPage",
-	"animal" => null,
-	"nextPages" => $previousPages,
-	"area" => "building"
-	);
-$pages[] = $beginPage;
-
-// reverse pages to start with begin and finish with end
-$pages = array_reverse($pages);
-
-if($visualDebug){
-// Display combination tree
-	foreach ($pages as $page) {
-		echo $page->name . " (" . $page->area . ")";
-		if($page->nextPages != null){
-			foreach ($page->nextPages as $next) {
-				echo "->" . $next->name . " (" . $next->area . ")";
-				echo "<br>";
-			}
-		}
-	}
+	// reverse pages to start with begin and finish with end
+	$pages = array_reverse($pages);
+	return $pages;
 }
 
 ?>
